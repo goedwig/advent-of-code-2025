035af7517325a518b7647bc08634ca93b9f14cb8
Alexander Tarasov <37976075+goedwig@users.noreply.github.com>
2025-12-05 18:11:16 +0200

    Drop redundant any_merged in day 5

--- 05/solution.py
+++ 05/solution.py
@@ -12,26 +12,17 @@ ranges.sort()
 
 def merge_ranges(ranges):
     merged_ranges = [ranges.pop(0)]
-    any_merged = False
-    while ranges:
-        r1 = merged_ranges[-1]
-        r2 = ranges.pop(0)
-
+    for r1 in ranges:
+        r2 = merged_ranges[-1]
         if r1[0] <= r2[1] and r2[0] <= r1[1]:
-            r3 = (min(r1[0], r2[0]), max(r1[1], r2[1]))
-            merged_ranges.pop(-1)
-            merged_ranges.append(r3)
-            any_merged = True
+            merged_ranges[-1] = (min(r1[0], r2[0]), max(r1[1], r2[1]))
         else:
-            merged_ranges.append(r2)
+            merged_ranges.append(r1)
 
-    return merged_ranges, any_merged
+    return merged_ranges
 
 
-while True:
-    ranges, any_merged = merge_ranges(ranges)
-    if not any_merged:
-        break
+ranges = merge_ranges(ranges)
 
 fresh = 0
 for i in ids:
